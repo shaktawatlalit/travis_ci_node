aa15d0ccbcbeeab1b5141333aa1ec324d9a2f340
lalit.shaktawat <lalit.shaktawat@RSPL-271>
2021-02-26 16:44:25 +0530

    travis

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,7 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://lalit:lalit&&123@cluster0-shard-00-00.grj7q.mongodb.net:27017,cluster0-shard-00-01.grj7q.mongodb.net:27017,cluster0-shard-00-02.grj7q.mongodb.net:27017/blog_dev?ssl=true&replicaSet=atlas-awz94d-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
+  redisUrl : "redis://127.0.0.1:6379"
 };
